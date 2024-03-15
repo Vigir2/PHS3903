553945d5e1b0c82fdd2533273a99a6d0aabd258b
Vigir2 <vincent-2.girouard@polymtl.ca>
2024-03-15 14:01:34 -0400

    Patates 4000

--- output_functions.py
+++ output_functions.py
@@ -1,11 +1,10 @@
 import numpy as np
 from datetime import datetime
 
-def write_xyz_file(l, fname=f"fuck_you.xyz"):
+def write_xyz_file(l, fname=f"Coords2.xyz"):
     with open(fname, "w") as f:
         f.write(f"{3*len(l)}\n\n")
         for m in l:
-            print(m)
             f.write(f"O     {'      '.join([str(m.O_pos[i]) for i in range(m.dim)])}\n")
             f.write(f"H     {'      '.join([str(m.H1_pos[i]) for i in range(m.dim)])}\n")
             f.write(f"H     {'      '.join([str(m.H2_pos[i]) for i in range(m.dim)])}\n")
@@ -14,7 +13,7 @@ def write_xyz_file(l, fname=f"fuck_you.xyz"):
 if __name__ == "__main__":
     from H2O import H2O
     m = []
-    for i in range(10):
+    for i in range(20):
         m.append(H2O(3))
         m[-1].rand_orientation()
         m[-1].rand_position()
--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-def write_pdb_file(output_file, atoms):
-    with open(output_file, 'w') as f:
-        f.write("HEADER    Sample PDB File\n")
-        atom_serial = 1
-        for atom in atoms:
-            f.write(f"ATOM  {atom_serial:5d} {atom['atom_name']:4s} MOL     1    {atom['x']:8.3f}{atom['y']:8.3f}{atom['z']:8.3f}  1.00  0.00           {atoms[at]}\n")
-            atom_serial += 1
-        f.write("END\n")
+import cv2
+import os
+
+
+image_folder = 'Animation/Frames'
+video_name = 'Animation/video.mp4'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".bmp")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 1, 24, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
