bbc00eb990b97d92fcf8314d0c735a355e84046b
vigir2 <vincent-2.girouard@polymtl.ca>
2024-04-27 18:35:57 -0400

    Bug fix

--- Tests/test2.py
+++ Tests/test2.py
@@ -1,7 +1,27 @@
+
+
 #!/usr/bin/env python3
 from functools import partial
 from itertools import repeat
 from multiprocessing import Pool, freeze_support
 import numpy as np
 
-print(type(None))
+def func(a, b):
+    return a + b, a
+
+def main():
+    a_args = [1,2,3]
+    second_arg = 1
+    with Pool() as pool:
+        L = pool.starmap(func, [(1, 1), (2, 1), (3, 1)])
+        M = pool.starmap(func, zip(a_args, repeat(second_arg)))
+        N = pool.map(partial(func, b=second_arg), a_args)
+        print(L)
+        L = np.array(L)
+        print(L[:,0])
+        print(L[:,1])
+
+
+if __name__=="__main__":
+    freeze_support()
+    main()
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import shutil
 import os
 import time
 
-def generate_equilibrium_state(name: str = None, N: int = None, T: float = None, P: float = None, a: float = None, n: int = 3) -> tuple:
+def generate_equilibrium_state(name: str = None, N: int = None, T: float = None, P: float = None, a: float = None, n: int = 7000) -> tuple:
     name_eq = name + f"_equilibrium_T{T}_P{P}"
     U = Universe(name=name_eq, N=N, a=a, T=T)
     U.npt_integration(dt=1.2, n=n, delta=2, T0=T, P0=P, Ewald=True, graphs=False, T=True, P=True, V=True)
@@ -15,7 +15,7 @@ def generate_equilibrium_state(name: str = None, N: int = None, T: float = None,
     out_state, a = U.get_state()
     return out_state, a
 
-def enthalpie_measurement(T: float, name: str, input_state: np.ndarray, a: float, P: float, n_npt: int = 6, n_nvt: int = 3) -> float:
+def enthalpie_measurement(T: float, name: str, input_state: np.ndarray, a: float, P: float, n_npt: int = 4000, n_nvt: int = 3000) -> float:
     name_npt = name + f"_npt_T{T}_P{P}"
     U = Universe(name=name_npt, a=a, input_state=input_state)
     data = U.npt_integration(dt=1, n=n_npt, delta=2, T0=T, P0=P, Ewald=True, graphs=False, H=True, P=True, T=True, V=True, E=True)
@@ -27,28 +27,44 @@ def enthalpie_measurement(T: float, name: str, input_state: np.ndarray, a: float
     data = U.nvt_integration(dt=1, n=n_nvt, delta=1, T0=T, P0=P, Ewald=True, graphs=False, H=True, P=True, T=True, V=True, E=True)
     shutil.move(os.path.join("Output", name_nvt), os.path.join("Output", name, "NVT"))
     H = np.mean(data["H"])
-    return H
+    T = np.mean(data["T"])
+    return H, T
 
 def H_vs_T(name: str, N: int, T: np.ndarray, P: float, a0: float):
     input_state, a = generate_equilibrium_state(name=name, N=N, T=T[0], P=P, a=a0)
     with Pool() as pool:
-        H = pool.starmap(enthalpie_measurement, zip(T, repeat(name), repeat(input_state), repeat(a), repeat(P)))
-    np.save(os.path.join("Output", name, "H_vs_T"), np.array(H))
-    np.save(os.path.join("Output", name, "T"), np.array(T))
+        Results = pool.starmap(enthalpie_measurement, zip(T, repeat(name), repeat(input_state), repeat(a), repeat(P)))
+    Results = np.array(Results)
+    np.save(os.path.join("Output", name, "H"), Results[:,0])
+    np.save(os.path.join("Output", name, "T"), Results[:,1])
+    return Results
+
+def only_nvt(T, name, P):
+    for file in os.listdir(os.path.join("Output", name, "NPT", name + f"_npt_T{T}_P{P}", "State_variables")):
+        if "Volume" in file:
+            V = np.load(os.path.join("Output", name, "NPT", name + f"_npt_T{T}_P{P}", "State_variables", file))
+            a = np.mean(V[len(V)//2:])**(1/3)
+    input_state = os.path.join("Output", name, "NPT", name + f"_npt_T{T}_P{P}", "state_log", name + f"_npt_T{T}_P{P}.npy")
+    name_nvt = name + f"_nvt_T{T}_P{P}"
+    U = Universe(name=name_nvt, a=a, input_state=input_state)
+    data = U.nvt_integration(dt=1, n=3000, delta=1, T0=T, P0=P, Ewald=True, graphs=False, H=True, P=True, T=True, V=True, E=True)
+    shutil.move(os.path.join("Output", name_nvt), os.path.join("Output", name, "NVT"))
+    H = np.mean(data["H"])
     return H
 
 if __name__ == "__main__":
-    name = "Eau_liquide_H_vs_T"
+    name = "Eau_liquide_H_vs_T_new"
     N = 100
     P = 10
     a0 = 18
     T = np.arange(300, 430, 10)
     print(T)
     tic = time.time()
-    H = H_vs_T(name=name, N=N, T=T, P=P, a0=a0)
+    Results = H_vs_T(name=name, N=N, T=T, P=P, a0=a0)
     toc = time.time()
     print(f"Time = {(toc-tic)/3600} h")
-    print(H)
+    print(Results[:,0])
+    print(Results[:,1])
 
     
 
